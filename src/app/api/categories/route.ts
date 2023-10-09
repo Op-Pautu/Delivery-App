ed155d8f11d3b02a5823d882f7315b9451a698b2
Op-Pautu <pautuop@gmail.com>
2023-10-09 22:17:52 +0530

    Add an api endpoint to fetch all categories

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export const GET = async () => {
+    try {
+        const categories = await prisma.category.findMany()
+        return new NextResponse(
+            JSON.stringify(categories),
+            { status: 200 }
+        )
+    } catch (error) {
+        return new NextResponse(JSON.stringify({ message: "Something went wrong" }), { status: 500 })
+    }
+}
