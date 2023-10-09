f04a95b7f2a966c87f506e9149dea78e27daaf84
Op-Pautu <pautuop@gmail.com>
2023-10-09 12:59:22 +0530

    Fix hydration errors

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,42 +1,42 @@
-import React from 'react'
-import Menu from './Menu'
-import Link from 'next/link'
-import CartIcon from './CartIcon'
-import Image from 'next/image'
+import React from "react";
+import Menu from "./Menu";
+import Link from "next/link";
+import CartIcon from "./CartIcon";
+import Image from "next/image";
 
 const Navbar = () => {
-  const user = false
+  const user = false;
   return (
-    <div className='h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 md:text-2xl lg:px-20 xl:px-40'>
-       {/* LEFT LINKS */}
+    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 md:text-2xl lg:px-20 xl:px-40">
+      {/* LEFT LINKS */}
       <div className="hidden md:flex gap-4 flex-1 lg:gap-12">
-        <Link href="/">Homepage</Link>
+        <Link href="/">Home</Link>
         <Link href="/menu">Menu</Link>
         <Link href="/">Contact</Link>
       </div>
       {/* LOGO */}
-      <div className='text-xl flex-1 md:font-bold md:text-center'>
-        <Link href='/'>
-          Massimo
-        </Link>
+      <div className="text-xl flex-1 md:font-bold md:text-center">
+        <Link href="/">Massimo</Link>
       </div>
       {/* MOBILE MENU */}
-      <div className='md:hidden'>
-        <Menu/>
+      <div className="md:hidden">
+        <Menu />
       </div>
       {/* RIGHT LINKS */}
       <div className="hidden md:flex gap-4 items-center flex-1 justify-end lg:gap-12">
-        <div className='flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md md:absolute top-3 right-2 lg:static'>
-          <Image src="/phone.png" alt='' width={20} height={20}/>
+        <div className="flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md md:absolute top-3 right-2 lg:static">
+          <Image src="/phone.png" alt="" width={20} height={20} />
           <span>123 456 89 </span>
         </div>
-        {!user ? <Link href="/login">Login</Link> :
-        <Link href="/orders">Orders</Link>
-       }
-       <CartIcon/>
+        {!user ? (
+          <Link href="/login">Login</Link>
+        ) : (
+          <Link href="/orders">Orders</Link>
+        )}
+        <CartIcon />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/app/components/Offer.tsx
+++ src/app/components/Offer.tsx
@@ -1,19 +1,32 @@
-import React from 'react'
-import CountDown from './Countdown';
-import Image from 'next/image';
+"use client";
+
+import React, { useEffect, useState } from "react";
+import CountDown from "./Countdown";
+import Image from "next/image";
 
 const Offer = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
   return (
     <div className="bg-black h-screen flex flex-col md:flex-row md:justify-between md:bg-[url('/offerBg.png')] md:h-[70vh]">
       {/* TEXT CONTAINER */}
       <div className="flex-1 flex flex-col justify-center items-center text-center gap-8 p-6">
-        <h1 className="text-white text-5xl font-bold xl:text-6xl">Delicious Burger & French Fry</h1>
+        <h1 className="text-white text-5xl font-bold xl:text-6xl">
+          Delicious Burger & French Fry
+        </h1>
         <p className="text-white xl:text-xl">
           Progressively simplify effective e-toilers and process-centric methods
           of empowerment. Quickly pontificate parallel.
         </p>
-        <CountDown/>
-        <button className="bg-red-500 text-white rounded-md py-3 px-6">Order Now</button>
+        <CountDown />
+        <button className="bg-red-500 text-white rounded-md py-3 px-6">
+          Order Now
+        </button>
       </div>
       {/* IMAGE CONTAINER */}
       <div className="flex-1 w-full relative md:h-full">
@@ -21,6 +34,6 @@ const Offer = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Offer
+export default Offer;
