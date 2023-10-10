5e764b19fc0ca25066b4e90c7d1a586159014ba8
Op-Pautu <pautuop@gmail.com>
2023-10-10 14:52:53 +0530

    Add auth using google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
 import Notification from "./components/Notification";
 import Footer from "./components/Footer";
+import AuthProvider from "./components/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,10 +22,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Notification />
-        <Navbar />
-        {children}
-        <Footer />
+        <AuthProvider>
+          <div>
+            <Notification />
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </AuthProvider>
       </body>
     </html>
   );
